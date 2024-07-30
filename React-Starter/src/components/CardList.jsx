52723393b604fa338a55261cfa0827c8eb4f5fed
Harsh Dagariya <103585690+harsh-recoverlution@users.noreply.github.com>
2024-07-30 13:15:18 +0530

    added grid

--- React-Starter/src/components/CardList.jsx
+++ React-Starter/src/components/CardList.jsx
@@ -1,11 +1,18 @@
 import CardEle from './Card'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 function CardList() {
     const data = [1,2,3,4,5,6,7,8,9]
     return (
-        <div className="card-list">
-            {data.map((item, index) => (
-                <CardEle key={index} title={item.title} description={item.description} />
-            ))}
+        <div className="container card-list">
+            <Row>
+                {data.map((item, index) => (
+                    <Col sm={3} key={index} className='mb-4'>
+                        <CardEle key={index} />
+                    </Col>
+                ))}
+            </Row>
         </div>
     );
 }
